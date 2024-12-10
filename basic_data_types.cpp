188fd1536d46d38324b8a50932acd50b17fa89d6
vandat <tranvandatkp@gmail.com>
2024-12-10 22:02:18 +0700

    Add basic_data_types.cpp file

---
+++ basic_data_types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=5;
+    unsigned int b=20;
+    float c= 3.14f;
+    double d=9.81;
+    char e='D';
+    bool f="false";
+    
+    cout<<"Interger:"<<a<<endl;
+    cout<<"Unsigned:"<<b<<endl;
+    cout<<"float:"<<c<<endl;
+    cout<<"double:"<<d<<endl;
+    cout<<"char:"<<e<<endl;
+    cout<<"bool:"<<f<<endl;
+
+
+}
